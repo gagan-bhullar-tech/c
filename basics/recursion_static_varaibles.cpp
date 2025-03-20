18e0ceed85908277b61cf1b26a568b4a154f9631
Gagan <g.deepsingh1@gmail.com>
2025-03-19 22:06:39 -0600

    static variables

---
+++ basics/recursion_static_varaibles.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+
+using namespace std;
+
+int sum(int n) {
+    static int i = 0;
+    if (n > 0) {
+        i++;
+        return i + sum(n - 1);
+    }
+    return 0;
+}
+
+int main() {
+    cout<<sum(5);
+    return 0;
+}
