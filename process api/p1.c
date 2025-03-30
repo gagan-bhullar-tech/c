27b34a0eb1cda6010198fb66e0d778fa210bb4db
Gagan <g.deepsingh1@gmail.com>
2025-03-30 00:21:15 -0600

    parent child process

---
+++ process api/p1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    printf("hello (pid: %d)\n", getpid());
+    int rc = fork();
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        // child process
+        printf("hello, I am child (pid: %d)\n", getpid());
+    } else {
+        // parent process
+        printf("hello, I am parent of %d (pid: %d)\n", rc, getpid());
+    }
+}
