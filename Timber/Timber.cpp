db62773e96dcf3f0fbe830614fcc281662d0650e
Gagan <g.deepsingh1@gmail.com>
2025-06-01 20:45:43 -0600

    add tree

--- Timber/Timber.cpp
+++ Timber/Timber.cpp
@@ -4,6 +4,9 @@ using namespace sf;
 
 int main()
 {
+	const float TREE_HORIZONTAL_POSITION = 810;
+	const float TREE_VERTICAL_POSITION = 0;
+
     VideoMode vm(1920, 1080);
 
 	RenderWindow window(vm, "Timber", Style::Fullscreen);
@@ -15,12 +18,20 @@ int main()
 	Sprite spriteBackground;
 	spriteBackground.setTexture(textureBackground);
 	spriteBackground.setPosition(0, 0);
+
+	Texture textureTree;
+
+	textureTree.loadFromFile("graphics/tree.png");
+	Sprite spriteTree;
+	spriteTree.setTexture(textureTree);
+	spriteTree.setPosition(TREE_HORIZONTAL_POSITION, TREE_VERTICAL_POSITION);
 	
 	while (window.isOpen()) {
 
 		window.clear();
 
 		window.draw(spriteBackground);
+		window.draw(spriteTree);
 
 		window.display();
 
