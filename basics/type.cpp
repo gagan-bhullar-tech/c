5645042f8cd91c4479e7354b9256db206222cb9f
Gagan <g.deepsingh1@gmail.com>
2025-12-14 11:41:40 -0700

    add type demonstration program with various integer initializations

---
+++ basics/type.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+int main() {
+    unsigned short a = 0b10101010;
+    printf("%hu\n", a);
+
+    int b = 0123;
+    printf("%d", b);
+
+    unsigned long long d = 0xFFFFFF;
+    printf("%d", d);
+}
