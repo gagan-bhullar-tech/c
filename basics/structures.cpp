679b5b1ae0a1b0a3f90d2bd0b4d9a5ca34dc43d5
Gagan <g.deepsingh1@gmail.com>
2025-12-29 21:14:38 -0700

    fix: add newline in output and ensure correct addition method usage in Point struct

--- basics/structures.cpp
+++ basics/structures.cpp
@@ -5,6 +5,10 @@ using namespace std;
 struct Point {
     int x;
     int y;
+
+    int add() {
+        return x + y;
+    }
 };
 
 void point(struct Point *p) {
@@ -16,5 +20,7 @@ int main() {
     Point p = {1, 2};
     struct Point *ptr = &p;
     point(ptr);
-    cout<<p.x<<" "<<p.y;
+    cout<<p.x<<" "<<p.y<<"\n";
+
+    printf("%d", p.add());
 }
