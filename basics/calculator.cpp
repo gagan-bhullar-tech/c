6c26bc38a3f3f64832837f7d6d33bc7016a742b0
Gagan <g.deepsingh1@gmail.com>
2025-12-30 21:10:48 -0700

    implement Calculator struct with basic arithmetic operations

---
+++ basics/calculator.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+enum class Operation {
+    Add,
+    Subtract,
+    Multiply,
+    Divide
+};
+
+struct Calculator {
+    Operation op;
+
+    Calculator(Operation operation) {
+        op = operation;
+    }
+
+    int calculate(int a, int b) {
+        switch (op) {
+            case Operation::Add: {
+                return a + b;
+            }
+            case Operation::Subtract: {
+                return a - b;
+            }
+            case Operation::Multiply: {
+                return a * b;
+            }
+            case Operation::Divide: {
+                return a / b;
+            }
+        }
+    }
+};
+
+int main() {
+    Calculator addCalc { Operation::Add };
+    printf("add: %d\n", addCalc.calculate(1, 9));
+
+    Calculator subCalc { Operation::Subtract };
+    printf("subtract: %d\n", subCalc.calculate(1, 9));
+
+    Calculator mulCalc { Operation::Multiply };
+    printf("multiply: %d\n", mulCalc.calculate(1, 9));
+
+    Calculator divCalc { Operation::Multiply };
+    printf("divide: %d\n", divCalc.calculate(9, 3));
+}
