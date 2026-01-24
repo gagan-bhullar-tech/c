77886195ce3e78d6205adaec98fee49cd6195a90
Gagan <g.deepsingh1@gmail.com>
2026-01-24 14:06:53 -0700

    fix: replace character throw with runtime_error in exception handling

--- basics/exception.cpp
+++ basics/exception.cpp
@@ -4,7 +4,7 @@
 struct Excep {
     void test(int x) {
         if (x == 0xFACE) {
-            throw 'a';
+            throw std::runtime_error("this is runtime error");
         }
         printf("it is not thrown");
     }
@@ -16,6 +16,7 @@ int main() {
         excep.test(0xFACE);
     } catch(const std::runtime_error &e) {
         printf("exception: %s", e.what());
+        throw;
     } catch(...) {
         printf("catch any exception");
     }
