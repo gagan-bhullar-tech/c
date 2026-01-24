ececeefc7deb4615abbff9e24c569ab2a4e90ef3
Gagan <g.deepsingh1@gmail.com>
2026-01-24 16:05:52 -0700

    add: implement assignment operator for SimpleString struct

--- basics/simple_string.cpp
+++ basics/simple_string.cpp
@@ -6,12 +6,25 @@ struct SimpleString {
     char *buffer;
     int length;
 
+    // this is copy constructor
     SimpleString(const SimpleString& other) :
         max_size { other.max_size},
         buffer { new char[other.max_size]},
         length { other.length } {
             std::strncpy(buffer, other.buffer, max_size);
         }
+
+    // this is assignment
+    SimpleString& operator = (const SimpleString& other) {
+        if (this == &other) return *this;
+        const auto new_buffer = new char[other.max_size];
+        delete[] buffer;
+        buffer = new_buffer;
+        length = other.length;
+        max_size = other.max_size;
+        std::strncpy(buffer, other.buffer, max_size);
+        return *this;
+    }
 
     ~SimpleString() {
         delete[] buffer;
