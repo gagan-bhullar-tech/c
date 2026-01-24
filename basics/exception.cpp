fe4c1dab70b9ff1ff4b1d5da313914f4e347c1e3
Gagan <g.deepsingh1@gmail.com>
2026-01-24 13:58:02 -0700

    catch any exception

--- basics/exception.cpp
+++ basics/exception.cpp
@@ -4,7 +4,7 @@
 struct Excep {
     void test(int x) {
         if (x == 0xFACE) {
-            throw std::runtime_error{ "this is an exception"};
+            throw 'a';
         }
         printf("it is not thrown");
     }
@@ -16,5 +16,7 @@ int main() {
         excep.test(0xFACE);
     } catch(const std::runtime_error &e) {
         printf("exception: %s", e.what());
+    } catch(...) {
+        printf("catch any exception");
     }
 }
